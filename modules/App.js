49e3ef9d32e625b3b0c1b1894b1461b8fa9fd08e
Ryan Florence <rpflorence@gmail.com>
2016-02-09 13:20:30 -0800

    Lesson 09 Final

--- modules/App.js
+++ modules/App.js
@@ -7,6 +7,7 @@ export default React.createClass({
       <div>
         <h1>React Router Tutorial</h1>
         <ul role="nav">
+          <li><NavLink to="/" onlyActiveOnIndex>Home</NavLink></li>
           <li><NavLink to="/about">About</NavLink></li>
           <li><NavLink to="/repos">Repos</NavLink></li>
         </ul>
