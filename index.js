04fc5fa4db520d4314671db5967e8e3665c67e2f
Ryan Florence <rpflorence@gmail.com>
2016-02-09 12:58:29 -0800

    Lesson 06 Final

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ import { Router, Route, hashHistory } from 'react-router'
 import App from './modules/App'
 import About from './modules/About'
 import Repos from './modules/Repos'
+import Repo from './modules/Repo'
 
 render((
   <Router history={hashHistory}>
     <Route path="/" component={App}>
       <Route path="/repos" component={Repos}/>
+      <Route path="/repos/:userName/:repoName" component={Repo}/>
       <Route path="/about" component={About}/>
     </Route>
   </Router>
--- modules/Repos.js
+++ modules/Repos.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 export default React.createClass({
   render() {
-    return <div>Repos</div>
+    return (
+      <div>
+        <h2>Repos</h2>
+        <ul>
+          <li><Link to="/repos/rackt/react-router">React Router</Link></li>
+          <li><Link to="/repos/facebook/react">React</Link></li>
+        </ul>
+      </div>
+    )
   }
 })
 
