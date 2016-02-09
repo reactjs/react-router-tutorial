7810c003313f4f1eed563b63ecdf2a855eb54854
Ryan Florence <rpflorence@gmail.com>
2016-02-09 15:01:01 -0800

    Lesson 10 Final

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Router, Route, hashHistory, IndexRoute } from 'react-router'
+import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 import App from './modules/App'
 import About from './modules/About'
 import Repos from './modules/Repos'
@@ -8,7 +8,7 @@ import Repo from './modules/Repo'
 import Home from './modules/Home'
 
 render((
-  <Router history={hashHistory}>
+  <Router history={browserHistory}>
     <Route path="/" component={App}>
       <IndexRoute component={Home}/>
       <Route path="/repos" component={Repos}>
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ module.exports = {
 
   output: {
     filename: 'bundle.js',
-    publicPath: ''
+    publicPath: '/'
   },
 
   module: {
