7f336847ce43679803a6debac627b33ba975ce54
Ryan Florence <rpflorence@gmail.com>
2016-02-09 16:07:41 -0800

    Lesson 11 Final

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
+var webpack = require('webpack')
+
 module.exports = {
   entry: './index.js',
 
   output: {
+    path: 'public',
     filename: 'bundle.js',
     publicPath: '/'
   },
+
+  plugins: process.env.NODE_ENV === 'production' ? [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin()
+  ] : [],
 
   module: {
     loaders: [
