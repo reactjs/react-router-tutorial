60bf84db61eddf480aec1dad97d3b72225775e76
sooham <rafizsooham@gmail.com>
2016-06-03 13:48:41 -0400

    Configured 01 to  work with Babel6 and fixed webpack-dev-server contentBase value

--- lessons/01-setting-up/webpack.config.js
+++ lessons/01-setting-up/webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
 
   module: {
     loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' }
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
     ]
   }
 }
