f463b0a191b0b1e71a2f17350c0b41da54f1e5f6
fridolph <249121486@qq.com>
2016-08-08 10:43:09 +0800

    12. navigation~

--- lessons/11-productionish-server/webpack.config.js
+++ lessons/11-productionish-server/webpack.config.js
@@ -2,6 +2,7 @@ module.exports = {
   entry: './index.js',
 
   output: {
+    path: 'public',
     filename: 'bundle.js',
     publicPath: '/'
   },
@@ -10,5 +11,13 @@ module.exports = {
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' }
     ]
-  }
+  },
+
+  // add this handful of plugins that optimize the build
+  // when we're in production
+  plugins: process.env.NODE_ENV === 'production' ? [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin()
+  ] : [],
 }
--- lessons/12-navigating/modules/Repos.js
+++ lessons/12-navigating/modules/Repos.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import { browserHistory } from 'react-router'
 import NavLink from './NavLink'
 
 export default React.createClass({
+  contextTypes: {
+    router: React.PropTypes.object
+  }, 
+
+  handleSubmit(event) {
+    event.preventDefault()    
+
+    const userName = event.target.elements[0].value
+    const repo = event.target.elements[1].value
+    const path = '/repos/${username}/${repo}'
+
+    browserHistory.push(path)
+    console.log(path);
+
+    this.context.router.push(path)
+  }, 
+
   render() {
     return (
       <div>
@@ -9,6 +27,14 @@ export default React.createClass({
         <ul>
           <li><NavLink to="/repos/reactjs/react-router">React Router</NavLink></li>
           <li><NavLink to="/repos/facebook/react">React</NavLink></li>
+          {/* add this form */}
+          <li>
+            <form onSubmit={this.handleSubmit}>
+              <input type="text" placeholder="userName" /> / {' '}
+              <input type="text" placeholder="repo" />{' '}
+              <button type="submit">Go</button>
+            </form>
+          </li>
         </ul>
         {this.props.children}
       </div>
